1dda27baf7c16c8ab538acdb69f3e62b3b88c592
stephen <henblackstep@gmail.com>
2022-02-24 22:47:49 +0000

    [UPDATE] init input component

--- src/app/components/UI/atoms/Checkbox.tsx
+++ src/app/components/UI/atoms/Checkbox.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
 
-interface CheckboxProps {}
+interface CheckboxProps {
+  size?: 'xs' | 'sm' | 'md' | 'lg'
+  label: string
+  state?: 'primary' | 'secondary' | 'accent'
+  disabled?: boolean
+  checked?: boolean
+  value?: string
+  onClick?: () => void
+}
 
-const Checkbox: React.FC<CheckboxProps> = ({}): JSX.Element => {
-  return <div>Checkbox</div>
+const Checkbox: React.FC<CheckboxProps> = ({
+  size = 'sm',
+  label,
+  state = 'primary',
+  checked,
+  disabled,
+  onClick,
+}): JSX.Element => {
+  return (
+    <div className="form-control">
+      <label className="label cursor-pointer">
+        <input
+          type="checkbox"
+          checked={checked}
+          className={`checkbox checkbox-${size} checkbox-${state}`}
+          disabled={disabled}
+          onClick={onClick}
+        />
+        <span className="label-text">{label}</span>
+      </label>
+    </div>
+  )
 }
 
 export default Checkbox
--- src/app/components/UI/atoms/Input.tsx
+++ src/app/components/UI/atoms/Input.tsx
@@ -1,9 +1,50 @@
 import React from 'react'
 
-interface InputProps {}
+interface InputProps {
+  label?: string
+  name: string
+  type: string
+  placeholder?: string
+  value?: string | number | readonly string[] | undefined
+  state?: 'primary' | 'secondary' | 'accent'
+  size?: 'xs' | 'sm' | 'md' | 'lg'
+  helperText?: string
+  bordered?: boolean
+  disabled?: boolean
+  onChange: () => void
+}
 
-const Input: React.FC<InputProps> = ({}): JSX.Element => {
-  return <div>Input</div>
+const Input: React.FC<InputProps> = ({
+  label,
+  name,
+  type,
+  placeholder,
+  value,
+  bordered = false,
+  size = 'md',
+  onChange,
+  helperText,
+  disabled,
+}): JSX.Element => {
+  return (
+    <div className="form-control">
+      <label className="label">
+        <span className="label-text">{label}</span>
+      </label>
+      <input
+        name={name}
+        value={value}
+        type={type}
+        placeholder={placeholder}
+        className={`input input-${size} ${bordered ? `input-bordered` : ''}`}
+        onChange={onChange}
+        disabled={disabled}
+      />
+      <label className="label">
+        <span className="label-text-alt">{helperText}</span>
+      </label>
+    </div>
+  )
 }
 
 export default Input
