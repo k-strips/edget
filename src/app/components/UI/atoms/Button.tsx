8fd258d00b79944228811269422230bc01465333
stephen <henblackstep@gmail.com>
2022-02-24 21:46:36 +0000

    [UPDATE] init button

--- src/app/components/UI/atoms/Button.tsx
+++ src/app/components/UI/atoms/Button.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
-interface ButtonProps {}
+interface ButtonProps {
+  disabled?: boolean
+  children: React.ReactNode
+  state?: 'info' | 'success' | 'warning' | 'error'
+  size: 'xs' | 'sm' | 'lg'
+  extraClass?: string
+  onClick?: (e: React.MouseEvent<HTMLElement>) => void
+}
 
-const Button: React.FC<ButtonProps> = ({}): JSX.Element => {
-  return <div>Button</div>
+const Button: React.FC<ButtonProps> = ({
+  children,
+  disabled,
+  state,
+  extraClass,
+  onClick,
+}): JSX.Element => {
+  return (
+    <button
+      className={`btn ${state ? `btn-${state}` : ''} ${extraClass}`}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  )
 }
 
 export default Button
--- src/app/components/layout/AuthLayout.tsx
+++ src/app/components/layout/AuthLayout.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-interface AuthLayoutProps {}
+interface AuthLayoutProps {
+  children: React.ReactNode
+}
 
-const AuthLayout: React.FC<AuthLayoutProps> = ({}): JSX.Element => {
-  return <div>AuthLayout</div>
+const AuthLayout: React.FC<AuthLayoutProps> = ({ children }): JSX.Element => {
+  return <div className="grid md:grid-cols-5 grid-cols-1">{children}</div>
 }
 
 export default AuthLayout
