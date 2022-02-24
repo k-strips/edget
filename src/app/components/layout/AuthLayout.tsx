1930c2f20bb9625c2386fe3bfc067f8d66137ab7
stephen <henblackstep@gmail.com>
2022-02-24 19:52:22 +0000

    [UPDATE] initial atoms,organisms and layout fstorybook files

---
+++ src/app/components/layout/AuthLayout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface AuthLayoutProps {}
+
+const AuthLayout: React.FC<AuthLayoutProps> = ({}): JSX.Element => {
+  return <div>AuthLayout</div>
+}
+
+export default AuthLayout
