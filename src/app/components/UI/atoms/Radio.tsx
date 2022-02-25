e644d1f8e8dae4ffa45a61f985df5bbb0c70233d
stephen <henblackstep@gmail.com>
2022-02-25 11:13:37 +0000

    [UPDATE] checkbox and button stories

--- src/app/components/UI/atoms/Radio.tsx
+++ src/app/components/UI/atoms/Radio.tsx
@@ -2,17 +2,44 @@ import React from 'react'
 
 interface RadioProps {
   checked: boolean
-  disabled: boolean
-  label: string
+  disabled?: boolean
+  label?: string
+  text?: string
+  name: string
+  value?: string
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
 const Radio: React.FC<RadioProps> = ({
   checked,
+  label,
   onChange,
   disabled,
+  name,
+  text,
+  value,
 }): JSX.Element => {
-  return <input>Radio</div>
+  const radioButton = (
+    <input
+      type="radio"
+      name={name}
+      id={name}
+      value={value}
+      onChange={onChange}
+      checked={checked}
+      disabled={disabled}
+    />
+  )
+  return label ? (
+    <div className="form-control">
+      <label className="cursor-pointer label">
+        {radioButton}
+        <span>{text}</span>
+      </label>
+    </div>
+  ) : (
+    radioButton
+  )
 }
 
 export default Radio
